3640235d8e7ece380de21b1872b9fa0e3f663f68
Mason Mitchell <masonmitchell0921@gmail.com>
2026-07-06 19:16:58 -0700

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unstuck
+//
+//  Created by Mason Mitchell on 7/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Get Unstuck!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnstuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnstuckApp.swift
+//  Unstuck
+//
+//  Created by Mason Mitchell on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct UnstuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
